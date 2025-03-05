1c4eff918f374b11eb4cf923ff5a84f7a2129ea3
MiriamASanchezC <miriamaliciasanchezcervantes@gmail.com>
2025-03-04 18:06:08 -0600

    juego

--- AppTrafficLights/ViewController.m
+++ AppTrafficLights/ViewController.m
@@ -16,7 +16,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    scoreInt = 0;
+    self.scoreLabel.text = [NSString stringWithFormat:@"%i",scoreInt];
+}
+-(void)startTimer{
+    timerInt -=1;
+    if (timerInt == 2) {
+        self.trafficLight.image = [UIImage imageNamed:@"trafficLight3"];
+    }else  if (timerInt == 1) {
+        self.trafficLight.image = [UIImage imageNamed:@"trafficLight2"];
+    }else  if (timerInt == 0) {
+        self.trafficLight.image = [UIImage imageNamed:@"trafficLight1"];
+        [timer invalidate];
+        //iniciar el timer del contador
+        scoreTimer = [NSTimer scheduledTimerWithTimeInterval:0.0001 target:self selector:@selector(scoreCounter) userInfo:nil repeats:YES];
+    }
+   
+    
+    
+    
+}
+-(void)scoreCounter{
+    scoreInt ++;
+    self.scoreLabel.text = [NSString stringWithFormat:@"%i",scoreInt];
+    
 }
 
-
+- (IBAction)startGame:(id)sender {
+    if (scoreInt == 0) {
+        timerInt = 3;
+        timer = [NSTimer scheduledTimerWithTimeInterval:1.0 target:self selector:@selector(startTimer) userInfo:nil repeats:YES];
+    }
+}
 @end
